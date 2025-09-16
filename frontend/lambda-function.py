3c9a51ea59c843064a4b6ad29df5ab69008e424f
ghidaa hasan <g.Hindieh@training.psut.edu.jo>
2025-09-16 17:16:26 +0300

    Updated lambda function to check voting result

--- frontend/lambda-function.py
+++ frontend/lambda-function.py
@@ -1,16 +1,24 @@
 import json
 import boto3
 
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('vote-table')  
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
+    
+    body = json.loads(event.get('body', '{}'))
     choice = body.get('choice', 'unknown')
-    
+
+  
+    print(f"Received vote: {choice}")
+
     table.put_item(Item={'Choice': choice})
-    
+
     return {
         'statusCode': 200,
-        'body': json.dumps({'message': f'Vote for {choice} recorded'})
+        'body': json.dumps({
+            'message': f'Vote for {choice} recorded',
+            'vote': choice
+        })
     }
